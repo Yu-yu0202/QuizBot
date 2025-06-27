462319bbdae9cf97ab462f364cfc174eb81e437e
ゆーゆー(yu-yu0202) <135312165+Yu-yu0202@users.noreply.github.com>
2025-06-27 21:08:39 +0900

    fix: utils/redis.ts

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -8,4 +8,9 @@ const redis = new Redis({
     db: Number(process.env.redis_db || 0),
 });
 export default redis;
-export const subscriber = redis;
+export const subscriber = new Redis({
+    host: process.env.redis_host,
+    port: Number(process.env.redis_port || 6379),
+    password: process.env.redis_password || undefined,
+    db: Number(process.env.redis_db || 0),
+});
